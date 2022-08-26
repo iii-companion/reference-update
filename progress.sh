076f1fae2d74ee8dec12599b4df00ec65ad18cf3
Will Haese-Hill <wahhill@gmail.com>
2022-08-26 15:13:09 +0100

    remove redundant files

--- progress.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env bash
-
-gene=$(tail -1 b.out | awk '{print $1}')
-line=$(grep -n $gene all_prots.fasta | cut -f1 -d:)
-total_lines=$(wc -l < all_prots.fasta)
-
-perc=$(bc <<< "scale=2; 100*$line/$total_lines")
-echo "${perc}%"
-
--- trainAugustus.sh
+++
@@ -1,4 +0,0 @@
-export AUGUSTUS_CONFIG_PATH=/home/whh2g/Bin/Augustus/config
-/home/whh2g/Bin/Augustus/scripts/new_species.pl --species=$1
-/home/whh2g/Bin/Augustus/scripts/gff2gbSmallDNA.pl $1.gff3 ../$1.fasta 1000 $1_sampled.gb
-etraining --species=$1 $1_sampled.gb
