95bf02c28b3d08ba751de6419079ed54c0bd01e9
Will Haese-Hill <wahhill@gmail.com>
2023-02-09 16:07:06 +0000

    improved OO chr parsing

--- bin/parse_chromosomes.py
+++ bin/parse_chromosomes.py
@@ -1,64 +1,111 @@
 #! /usr/bin/env python
 from collections import Counter
-from itertools import zip_longest
+from itertools import groupby, zip_longest
+from operator import itemgetter
 import os
 import re
 import sys
 
-patterns = ["[0-9][0-9]", "[0-9]",]
+patterns = [
+    "([0-9][0-9])",
+    "([0-9][0-9]){0,1}(MIT|API){0,1}",
+    "([0-9])",
+    "([MDCLXVI]+[a-z]?)"
+]
 
+class ChromosomeParser:
+    def __init__(self, gff) -> None:
+        self.species = ''
+        self.prefix = ''
+        self.pattern = ''
+        self.suffix = ''
+        self.sequence_regions = []
+        self.chr_candidates = []
+        self._parse(gff)
+    
+    @property
+    def regex(self) -> str:
+        return "{}{}{}".format(self.prefix, self.pattern, self.suffix)
+    
+    def __repr__(self) -> str:
+        return "\t".join(filter(None, (self.species, self.regex)))
+    
+    def _parse(self, gff) -> None:
+        self.species, _ = os.path.splitext(os.path.basename(gff))
+        with open(gff, 'rt') as f:
+            data = f.readlines()
+        self.sequence_regions = [list(filter(None, l.split(" ")))[1] for l in data if l.startswith("##sequence-region")]
+        self.chr_candidates = [r for r in self.sequence_regions if {".", "_"}.intersection(r)]
+    
+    def generate_regex(self) -> None:
+        self.has_suffix()
+        prefix_pool = self._iter_prefix_pool()
+        while self.pattern == "":
+            max_match = 0
+            try:
+                prefix, freq = next(prefix_pool)
+            except StopIteration:
+                # self.pattern = ".*"
+                # self.prefix, _, _ = next(self._iter_prefix_pool())
+                raise
+            if prefix and not (freq == 1 and len(self.sequence_regions) > 1):
+                r = re.compile(r"{}.*{}$".format(prefix, self.suffix), re.IGNORECASE)
+                filt_candidates = list(filter(r.match, self.chr_candidates))
+                for pattern in patterns:
+                    r = re.compile(r"{}{}{}$".format(prefix, pattern, self.suffix), re.IGNORECASE)
+                    len_match = len(list(filter(r.match, filt_candidates)))
+                    if len_match > max_match:
+                        max_match = len_match
+                        self.prefix = prefix
+                        self.pattern = pattern
+        
+    def has_suffix(self) -> bool:
+        """
+        Assume any sequence_region with a version number "_v#" is a likely candidate for a chromosome.
+        Filter candidates accordingly.
+        """
+        with_suffix = list(filter(re.compile(r".*_v\d+$").match, self.chr_candidates))
+        if any(with_suffix):
+            self.chr_candidates = with_suffix
+            self.suffix = re.search(r"_v\d+", with_suffix[0]).group()
+            return True
+        return False
+    
+    def _iter_prefix_pool(self):
+        yield from common_prefixes(self.chr_candidates)
+        # yield from [
+        #     common_prefixes(self.chr_candidates),
+        #     (self.chr_candidates[0].split("_")[0] + "_", 1),
+        # ]
+    
+    def correct_prefix(self):
+        r = re.compile(r"{}$".format(self.regex), re.IGNORECASE)
+        first_match = list(filter(r.match, self.chr_candidates))[0]
+        prefix = re.split('_|\.', first_match)[0]
+        separator = first_match.split(prefix)[1][0]
+        self.prefix = prefix + separator
 
 def common_prefixes(li):
-    threshold = len(li)
-    prefix = []
     prefixes = []
-    for chars in zip_longest(*li, fillvalue=''):
-        char, count = Counter(chars).most_common(1)[0]
-        if count == 1:
-            break
-        elif count < threshold:
-            if prefix:
-                prefixes.append((''.join(prefix), threshold))
-            threshold = count
-        prefix.append(char)
-    if prefix:
-        prefixes.append((''.join(prefix), threshold))
-    return prefixes
-
-
-def parse_chromosomes(gff):
-    species, _ = os.path.splitext(os.path.basename(gff))
-    regex = ""
-    with open(gff, 'rt') as f:
-        data = f.readlines()
-    regions = [list(filter(None, l.split(" ")))[1] for l in data if l.startswith("##sequence-region")]
-    chromosomes = [r for r in regions if "_" in r]
-    if chromosomes:
-        with_suffix = list(filter(re.compile(r".*_v\d+$").match, chromosomes))
-        if len(with_suffix) > 1:
-            prefix, freq = common_prefixes(with_suffix)[0]
-        elif len(chromosomes) > 1:
-            prefix, freq = common_prefixes(chromosomes)[0]
-        else:
-            prefix, freq = chromosomes[0].split("_")[0] + "_", 1
-        if prefix and not (freq == 1 and len(regions) > 1):
-            suffix = re.search(r"_v\d+", with_suffix[0]).group() if any(with_suffix) else ""
-            r = re.compile(r"{}.*{}$".format(prefix, suffix))
-            filt_chromosomes = list(filter(r.match, chromosomes))
-            max_match = 0
-            best_pattern = ""
-            for pattern in patterns:
-                r = re.compile(r"{}{}{}$".format(prefix, pattern, suffix))
-                len_match = len(list(filter(r.match, filt_chromosomes)))
-                if len_match > max_match:
-                    max_match = len_match
-                    best_pattern = pattern
-            if not best_pattern:
-                best_pattern = ".*"
-            regex = "{}({}){}".format(prefix, best_pattern, suffix)
-    return "\t".join(filter(None, (species, regex)))
+    for first_letter, prefix_batch in groupby(sorted(li), key=itemgetter(0)):
+        threshold = len(li)
+        prefix = []
+        for chars in zip_longest(*map(str.lower, prefix_batch), fillvalue=''):
+            char, count = Counter(chars).most_common(1)[0]
+            if count == 1:
+                break
+            elif count < threshold:
+                if prefix:
+                    prefixes.append((''.join(prefix), threshold))
+                    break
+                threshold = count
+            prefix.append(char)    
+    return sorted(prefixes, key=itemgetter(1))
 
 
 if __name__ == "__main__":
     gff = sys.argv[1]
-    print(parse_chromosomes(gff))
+    parser = ChromosomeParser(gff)
+    parser.generate_regex()
+    parser.correct_prefix()
+    print(parser)
--- test/test.py
+++ test/test.py
@@ -2,14 +2,19 @@ import glob
 import os.path
 import unittest
 
-from bin.parse_chromosomes import parse_chromosomes
+from bin.parse_chromosomes import ChromosomeParser
 
 
 expected_pattern = {
-    "pviv.gff3": "PvP01_([0-9][0-9])_v2",
+    "pviv.gff3": "PvP01_([0-9][0-9]){0,1}(MIT|API){0,1}_v2",
     "padl.gff3": "PADLG01_([0-9][0-9])",
-    "pfal.gff3": "Pf3D7_([0-9][0-9])_v3",
-    "tgon.gff3": "TGME49_chr(.*)",
+    "pfal.gff3": "Pf3D7_([0-9][0-9]){0,1}(MIT|API){0,1}_v3",
+    "tgon.gff3": "TGME49_([MDCLXVI]+[a-z]?)",
+    "ltro.gff3": "LtrL590_([0-9][0-9])",
+    "lmxm.gff3": "LmxM.([0-9][0-9])",
+    "lael.gff3": "LaeL147_([0-9][0-9])",
+    "larl.gff3": "LarLEM1108_([0-9][0-9])",
+    "prel.gff3": "PRELSG_([0-9][0-9]){0,1}(MIT|API){0,1}_v1"
 }
 
 class TestParseChromosomes(unittest.TestCase):
@@ -22,10 +27,12 @@ class TestParseChromosomes(unittest.TestCase):
     
     def test_parse_examples(self):
         for tf in self.test_files:
-            p = parse_chromosomes(tf)
+            p = ChromosomeParser(tf)
+            p.generate_regex()
+            p.correct_prefix()
             try:
-                self.assertEqual(len(p.split("\t")), 2)
-                self.assertEqual(p.split("\t")[1], expected_pattern[os.path.basename(tf)])
+                self.assertEqual(len(str(p).split("\t")), 2)
+                self.assertEqual(str(p).split("\t")[1], expected_pattern[os.path.basename(tf)])
             except AssertionError as e:
                 self.verification_errors.append(str(e))
 
