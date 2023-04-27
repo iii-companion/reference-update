2367cd4d2df3ca496fb746c7ddb6ffdd0d977070
Will Haese-Hill <wahhill@gmail.com>
2023-04-27 11:43:18 +0100

    lint

--- test/test.py
+++ test/test.py
@@ -17,14 +17,15 @@ expected_pattern = {
     "prel.gff3": "PRELSG_([0-9][0-9]){0,1}(MIT|API){0,1}_v1"
 }
 
+
 class TestParseChromosomes(unittest.TestCase):
     def setUp(self):
         self.test_files = glob.glob("test/*.gff3")
         self.verification_errors = []
-    
+
     def tearDown(self):
         self.assertEqual([], self.verification_errors)
-    
+
     def test_parse_examples(self):
         for tf in self.test_files:
             p = ChromosomeParser(tf)
@@ -32,7 +33,8 @@ class TestParseChromosomes(unittest.TestCase):
             p.correct_prefix()
             try:
                 self.assertEqual(len(str(p).split("\t")), 2)
-                self.assertEqual(str(p).split("\t")[1], expected_pattern[os.path.basename(tf)])
+                self.assertEqual(str(p).split("\t")[1],
+                                 expected_pattern[os.path.basename(tf)])
             except AssertionError as e:
                 self.verification_errors.append(str(e))
 
