932ef79be1f9810049e974fad3031059fe69aa4e
Will Haese-Hill <wahhill@gmail.com>
2023-05-17 16:02:09 +0100

    generic outdir for run_all

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 configsDir=$1
-refDir=$2
+outDir=$2
 
 # For configs dir containing files of form "params_X.config", each producing a reference dir "ref_X"
 for x in $configsDir/*
 do
     nextflow run reference_update.nf -c $x -with-trace -qs 8
-    echo $x | sed -e "s/^configs\/params/ref/" -e "s/.config$//" | xargs mv -t $refDir/backup
+    echo $x | sed -e "s/^configs\/params/ref/" -e "s/.config$//" | xargs mv -t $outDir
 done
